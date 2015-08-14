77b7496626f1d62b41b54d0ea4156c73e21552ca
Jean Carlos Klann <jeanklann@hotmail.com>
2015-08-13 21:12:19 -0300

    Check-in inicial do módulo ProjetoBD2

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ProjetoBD2 {
+    class MainClass {
+        public static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
