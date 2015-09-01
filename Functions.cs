b8f5a1514ae828a56b859159a5e10688a76f4019
Jean Carlos Klann <jeanklann@hotmail.com>
2015-09-01 00:26:13 -0300

    Melhorado a conversão string

--- Functions.cs
+++ Functions.cs
@@ -1,21 +1,16 @@
 ﻿using System;
+using System.Text;
 
 namespace ProjetoBD2 {
     public static class Functions {
         public static byte[] GetBytes(string str)
         {
-            byte[] bytes = new byte[str.Length];
-            for(int i = 0; i < bytes.Length; i++) {
-                bytes[i] = (byte)str[i];
-            }
-            return bytes;
+            return Encoding.ASCII.GetBytes(str);
         }
 
         public static string GetString(byte[] bytes)
         {
-            char[] chars = new char[bytes.Length];
-            Array.Copy(bytes, chars, bytes.Length);
-            return new string(chars);
+            return Encoding.ASCII.GetString(bytes);
         }
     }
 }
